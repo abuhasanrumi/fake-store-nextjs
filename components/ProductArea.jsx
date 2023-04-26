5531c0f015feae1ed04a2d13218384d03e1199dd
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-26 07:28:30 +0600

    Added product area, implemented product card, responsive

--- components/ProductArea.jsx
+++ components/ProductArea.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
 
 const ProductArea = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios
-      .get("https://fakestoreapi.com/products")
+      .get("https://fakestoreapi.com/products?limit=12")
       .then((response) => {
         setData(response.data);
       })
@@ -14,7 +15,13 @@ const ProductArea = () => {
       });
   }, []);
 
-  return <div>{console.log(data)}</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+      {data?.map((product) => (
+        <ProductCard key={product?.id} data={product} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductArea;
--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({ data }) => {
+  const { id, title, category, description, image, rating, price } = data;
+  return (
+    <Link
+      href={`/product/${id}`}
+      className="transform overflow-hidden duration-200 hover:scale-105 cursor-pointer"
+    >
+      <div className="relative h-[300px] p-6 bg-gray-100 ">
+        <Image
+          className="object-contain mx-auto h-full"
+          src={image}
+          alt={title}
+          height={300}
+          width={300}
+        />
+      </div>
+      <div className="p-4 text-black/[0.9] flex justify-between gap-10">
+        <h2 className="text-lg font-medium">{title}</h2>
+        <p className="mr-2 text-xl font-semibold text-red-600">${price}</p>
+      </div>
+    </Link>
+  );
 };
 
 export default ProductCard;
