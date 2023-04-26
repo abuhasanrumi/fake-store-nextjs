4e2a569a9b661f6110ca7e662c29d8cbef8e4ee7
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-26 08:58:18 +0600

    Added a not found page for 404 error pages

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Intro = () => {
+const NotFound = () => {
   return (
-    <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+    <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[170px]">
       <div className="text-[28px] md:text-[34px] mb-10 font-semibold">
-        Lorem ipsum dolor sit amet.
+        Page Not Found
       </div>
       <div className="text-md md:text-xl">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quam
@@ -14,4 +14,4 @@ const Intro = () => {
   );
 };
 
-export default Intro;
+export default NotFound;
